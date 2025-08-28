6cd573558ef14c884db2a8c4236a269c996e82e2
Khuu Trong Quan <57951538+khuutrongquan@users.noreply.github.com>
2025-08-28 21:58:01 +0700

    Update embed.py

--- model/embed.py
+++ model/embed.py
@@ -77,7 +77,7 @@ class TemporalConvNet(nn.Module):
         return x
 
 class TokenEmbedding(nn.Module):
-    def __init__(self, c_in, d_model, num_channels=[32, 64, 128], kernel_size=3, dropout=0.0):
+    def __init__(self, c_in, d_model, num_channels=[32, 64, 128], kernel_size=2, dropout=0.0):
         super(TokenEmbedding, self).__init__()
         self.tcn = TemporalConvNet(c_in, num_channels, kernel_size=kernel_size, dropout=dropout)
         self.linear = nn.Linear(num_channels[-1], d_model)
