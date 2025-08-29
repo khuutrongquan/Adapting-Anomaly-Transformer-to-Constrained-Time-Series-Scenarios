9643486b55d401f4a2adcf05e889a269b761cb76
Khuu Trong Quan <khuutrongquan220405@gmail.com>
2025-08-29 21:58:06 +0700

    Review update

--- model/embed.py
+++ model/embed.py
@@ -57,7 +57,7 @@ class TemporalBlock(nn.Module):
         return self.act(out + res)
     
 class TemporalConvNet(nn.Module):
-    def __init__(self, num_inputs, num_channels=[32, 64, 128], kernel_size=2, dropout=0.0, activation='relu'):
+    def __init__(self, num_inputs, num_channels=[32, 64, 128], kernel_size=3, dropout=0.0, activation='relu'):
         super(TemporalConvNet, self).__init__()
         self.n_inputs = num_inputs
         layers = []
@@ -76,9 +76,9 @@ class TemporalConvNet(nn.Module):
         x = self.network(x)
         return x
 
-class TokenEmbedding(nn.Module):
+class TCNEmbedding(nn.Module):
     def __init__(self, c_in, d_model, num_channels=[32, 64, 128], kernel_size=3, dropout=0.0):
-        super(TokenEmbedding, self).__init__()
+        super(TCNEmbedding, self).__init__()
         self.tcn = TemporalConvNet(c_in, num_channels, kernel_size=kernel_size, dropout=dropout)
         self.linear = nn.Linear(num_channels[-1], d_model)
         self.init_weights()
@@ -104,6 +104,87 @@ class ConvolutionalEmbedding(nn.Module):
     def forward(self, x):
         x = self.tokenConv(x.permute(0, 2, 1)).transpose(1, 2)
         return x
+    
+class TokenEmbedding(nn.Module):
+    def __init__(self, c_in, d_model):
+        super(TokenEmbedding, self).__init__()
+        self.tcn = TCNEmbedding(c_in, d_model)
+        self.cnn = ConvolutionalEmbedding(c_in, d_model)
+
+    def forward(self, x):
+        x = self.tcn(x) + self.cnn(x)
+        return x
+
+class DoubleSequentialConvolutionalEmbedding(nn.Module):
+    def __init__(self, c_in, d_model):
+        super(DoubleSequentialConvolutionalEmbedding, self).__init__()
+        self.tokenConv1 = nn.Conv1d(in_channels=c_in, out_channels=d_model,
+                                   kernel_size=3, padding=1, bias=False)
+        self.tokenConv2 = nn.Conv1d(in_channels=d_model, out_channels=d_model,
+                                   kernel_size=3, padding=1, bias=False)
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
+
+    def forward(self, x):
+        x = self.tokenConv1(x.permute(0, 2, 1))
+        x = F.leaky_relu(x)
+        x = self.tokenConv2(x).transpose(1, 2)
+        return x
+
+class DoubleSequentialTCNEmbedding(nn.Module):
+    def __init__(self, c_in, d_model, num_channels=[32, 64, 128], kernel_size=3, dropout=0.0):
+        super(DoubleSequentialTCNEmbedding, self).__init__()
+        self.tcn1 = TemporalConvNet(c_in, num_channels, kernel_size=kernel_size, dropout=dropout)
+        self.tcn2 = TemporalConvNet(num_channels[-1], num_channels, kernel_size=kernel_size, dropout=dropout)
+        self.linear = nn.Linear(num_channels[-1], d_model)
+        self.init_weights()
+
+    def init_weights(self):
+        self.linear.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+        out = self.tcn1(x)                          # [B, C_out, L]
+        out = self.tcn2(out)                        # [B, C_out, L]
+        out = out.transpose(1, 2)                   # [B, L, C_out]
+        return self.linear(out)                     # [B, L, d_model]
+
+class DoubleParallelConvolutionalEmbedding(nn.Module):
+    def __init__(self, c_in, d_model):
+        super(DoubleParallelConvolutionalEmbedding, self).__init__()
+        self.tokenConv1 = nn.Conv1d(in_channels=c_in, out_channels=d_model,
+                                   kernel_size=3, padding=1, bias=False)
+        self.tokenConv2 = nn.Conv1d(in_channels=d_model, out_channels=d_model,
+                                   kernel_size=3, padding=1, bias=False)
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
+
+    def forward(self, x):
+        x1 = self.tokenConv1(x.permute(0, 2, 1))    # [B, d_model, L]
+        x2 = self.tokenConv1(x.permute(0, 2, 1))    # [B, d_model, L]
+        out = x1 + x2                               # [B, d_model, L]
+        return out.permute(0, 2, 1)                 # [B, L, d_model]
+    
+class DoubleParallelTCNEmbedding(nn.Module):
+    def __init__(self, c_in, d_model, num_channels=[32, 64, 128], kernel_size=3, dropout=0.0):
+        super(DoubleParallelTCNEmbedding, self).__init__()
+        self.tcn1 = TemporalConvNet(c_in, num_channels, kernel_size=kernel_size, dropout=dropout)
+        self.tcn2 = TemporalConvNet(c_in, num_channels, kernel_size=kernel_size, dropout=dropout)
+        self.linear = nn.Linear(num_channels[-1], d_model)
+        self.init_weights()
+
+    def init_weights(self):
+        self.linear.weight.data.normal_(0, 0.01)
+
+    def forward(self, x):
+        x = x.permute(0, 2, 1)
+        y1 = self.tcn1(x)                           # output: [B, C_out, L]
+        y2 = self.tcn2(x)                           # output: [B, C_out, L]
+        y = y1 + y2
+        y = y.transpose(1, 2)                       # → [B, L, C_out]
+        return self.linear(y)                       # → [B, L, d_model]
 
 class PositionalEmbedding(nn.Module):
     def __init__(self, d_model, max_len=5000):
@@ -128,13 +209,15 @@ class PositionalEmbedding(nn.Module):
 class DataEmbedding(nn.Module):
     def __init__(self, c_in, d_model, dropout=0.0):
         super(DataEmbedding, self).__init__()
-
-        self.value_embedding = TokenEmbedding(c_in=c_in, d_model=d_model)
-        self.conv_embedding = ConvolutionalEmbedding(c_in=c_in, d_model=d_model)
+        # self.value_embedding = TokenEmbedding(c_in=c_in, d_model=d_model)
+        # self.value_embedding = DoubleParallelTCNEmbedding(c_in=c_in, d_model=d_model)
+        # self.value_embedding = DoubleParallelConvolutionalEmbedding(c_in=c_in, d_model=d_model)
+        self.value_embedding = DoubleSequentialTCNEmbedding(c_in=c_in, d_model=d_model)
+        # self.value_embedding = DoubleSequentialConvolutionalEmbedding(c_in=c_in, d_model=d_model)
         self.position_embedding = PositionalEmbedding(d_model=d_model)
 
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x):
-        x = self.value_embedding(x) + self.conv_embedding(x) + self.position_embedding(x)
+        x = self.value_embedding(x) + self.position_embedding(x)
         return self.dropout(x)
